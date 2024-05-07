d9238740a4a7753994bf804e58d3e208da09abd0
unknown <aditiharishjpn@gmail.com>
2024-05-07 18:03:03 +0530

    Complete the test class for addFive

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,34 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class GHappyTest {
+    @Test
+    public void testAllGHappy() {
+        GHappy gHappy = new GHappy();
+        assertTrue(gHappy.gHappy("xxggxx"));
+        assertTrue(gHappy.gHappy("xggxggx"));
+        assertTrue(gHappy.gHappy("ggggg"));
+    }
+
+    @Test
+    public void testUnhappyG() {
+        GHappy gHappy = new GHappy();
+        assertFalse(gHappy.gHappy("xxgxx"));
+        assertFalse(gHappy.gHappy("xxggyygxx"));
+        assertFalse(gHappy.gHappy("ggxxg"));
+    }
+
+    @Test
+    public void testNoG() {
+        GHappy gHappy = new GHappy();
+        assertTrue(gHappy.gHappy("xxxxxx"));
+        assertTrue(gHappy.gHappy(""));
+    }
+
+   
+
 }
